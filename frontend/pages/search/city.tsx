7813b4a1195b0556c95311db206586d5aac09cd1
setodeve <keinick11@outlook.com>
2024-06-15 02:47:39 +0900

    feat: add search pages

---
+++ frontend/pages/search/city.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Town from '@/components/Town'
+
+interface Location {
+  town: string
+  koaza: string
+  lat: number
+  lng: number
+}
+
+const Home: React.FC = () => {
+  const router = useRouter()
+  const [town, setTown] = useState<Location[] | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pref = router.query.pref as string
+      const city = router.query.city as string
+      if (pref === undefined || city === undefined) {
+        return
+      }
+      const res = await fetch(
+        'https://geolonia.github.io/japanese-addresses/api/ja/' + pref + '/' + city + '.json',
+      )
+      const result = await res.json()
+      setTown(result)
+    }
+    fetchData()
+  }, [router, router.query.pref, router.query.city])
+
+  if (!town || town?.length === 0) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <Town
+      pref={router.query.pref as string}
+      city={router.query.city as string}
+      townes={town as Location[]}
+    />
+  )
+}
+
+export default Home
