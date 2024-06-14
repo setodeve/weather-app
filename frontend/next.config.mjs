31cc5c27afe599d88158131eb8a490a12c256895
setodeve <keinick11@outlook.com>
2024-06-14 20:10:28 +0900

    feat: add eslint,prettier

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
+}
+
 
 export default nextConfig;
