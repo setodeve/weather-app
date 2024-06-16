480bfee171906cf505177179b59bb53c21f2200b
setodeve <keinick11@outlook.com>
2024-06-17 08:58:47 +0900

    fix : fix weather page

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,5 +1,12 @@
 import GoogleMapReact from 'google-map-react'
 import React from 'react'
+// import { Loader } from '@googlemaps/js-api-loader';
+
+// const loader = new Loader({
+//     apiKey: process.env['NEXT_PUBLIC_GOOGLE_MAP_API_KEY'] as any,
+//     version: "weekly",
+//     libraries: ["places"]
+// });
 
 interface LatLngEvent {
   x: number
@@ -9,26 +16,41 @@ interface LatLngEvent {
   event: Event
 }
 
-export default function Create() {
+export default function Map({ lat, lng }: { lat: number; lng: number }) {
   const defaultLatLng = {
-    lat: 35.7022589,
-    lng: 139.7744733,
+    lat: lat,
+    lng: lng,
   }
-
-  const APIKEY = ''
 
   const setLatLng = ({ x, y, lat, lng, event }: LatLngEvent): void => {
     console.log(lat)
     console.log(lng)
   }
 
+  const renderMarkers = (map: any, maps: any) => {
+    // loader.load().then((google)=>{
+    //   new maps.Marker({
+    //     position: {
+    //       lat: 35.6895,
+    //       lng: 139.6923
+    //     },
+    //     map
+    // })
+    new maps.Marker({
+      position: defaultLatLng,
+      map,
+    })
+  }
+
   return (
     <div style={{ width: '100%', height: '400px' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: APIKEY }}
+        bootstrapURLKeys={{ key: process.env['NEXT_PUBLIC_GOOGLE_MAP_API_KEY'] as any }}
         defaultCenter={defaultLatLng}
         defaultZoom={8}
-        onClick={setLatLng}
+        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+        // onClick={setLatLng}
+        yesIWantToUseGoogleMapApiInternals
       ></GoogleMapReact>
     </div>
   )
